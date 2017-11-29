ad64a628b062d50b60028e50a53acb3f463a4697
kale-io <cos+a@iuvo.ai>
2017-11-28 21:47:18 -0800

    Enforce explicit boolean attributes notation in JSX When using a boolean attribute in JSX, you can set the attribute value to true or omit the value. This rule will add an explicit `true` value to attributes with ommited values.

--- examples/using-remark/src/html.js
+++ examples/using-remark/src/html.js
@@ -31,13 +31,13 @@ class HTML extends React.Component {
             rel="preload"
             href={`/static/spectral-latin-400.bc2de9de.woff2`}
             as="font"
-            crossOrigin={true}
+            crossOrigin
           />
           <link
             rel="preload"
             href={`/static/spectral-latin-800.53eca5bf.woff2`}
             as="font"
-            crossOrigin
+            crossOrigin={true}
           />
           <link
             rel="prefetch"
